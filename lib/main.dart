5c638affebc332f8d3d5a2ff9efd4281788422bf
Keisuke2162 <applabo.kei@gmail.com>
2022-01-15 17:41:15 +0900

    debugバナー削除

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
